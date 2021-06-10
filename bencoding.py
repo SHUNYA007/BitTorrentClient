c4c4416445b9c146ac181d359800c67a0422119d
SHUNYA007 <prashantsingh7@protonmail.com>
2021-06-10 05:44:02 +0530

    Created Decoder

--- bencoding.py
+++ bencoding.py
@@ -1,6 +1,3 @@
-import re
-
-
 class Decoder:
 
     """
@@ -11,24 +8,59 @@ class Decoder:
             raise TypeError('Argument is not of type "bytes" ')
         self.data=data.decode() #converting bytes to string type
         self.index=-1
+        self.character=None
+
     def getCharacter(self):
         self.index+=1
-        if self.index>len(self.data):
-            return None
-        return self.data[self.index]
+        if self.index>=len(self.data):
+            self.character=None
+        else:
+            self.character= self.data[self.index]
+    def getLength(self):
+        lenOfString=''
+        while self.character!=':':
+            lenOfString+=self.character
+            self.getCharacter()
+
+        return int(lenOfString)
+    def getString(self,lenOfString):
+        output=''
+        for _ in range(lenOfString):
+            self.getCharacter()
+            output+=self.character
+
+        return output
+
     def decode(self):
-        character=self.getCharacter()
-        if character == None:
+        self.getCharacter()
+        if self.character == None:
             raise EOFerror('Unexpected end of file')
-        elif character == i:
+        elif self.character == 'i':
             ##code for integerInput
-            pass
-        elif character == d:
+            self.getCharacter()
+            output=''
+            while self.character!='e':
+                output+=self.character
+                self.getCharacter()
+            return int(output)
+        elif self.character == 'd':
             ##code for dictionary input
-            pass
-        elif character == l:
+            output={}
+            while self.character!='e':
+                key=self.decode()
+                if key!=None:
+                    value=self.decode()
+                    output[key]=value
+            return output
+        elif self.character == 'l':
             ##code for list input
-            pass
-        elif character in '0123456789':
+            output=[]
+            while self.character!='e':
+                item=self.decode()
+                if item!=None:
+                    output.append(item)
+            return output
+        elif self.character in '0123456789':
             ##code for a string input
-            pass
+            lenOfString=self.getLength()
+            return self.getString(lenOfString)
